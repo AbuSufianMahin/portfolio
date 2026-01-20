e374f550af8c4fb230828bf4c824d25649a55d86
Abu Sufian Mahin <abusufianmahin3@gmail.com>
2026-01-20 20:29:34 +0600

    fixed resume button

--- src/Components/Common/ResumeButton.jsx
+++ src/Components/Common/ResumeButton.jsx
@@ -7,7 +7,7 @@ const ResumeButton = () => {
             href="/sufian_resume.pdf"
             download
         >
-            <Button className="rounded-full border-2 border-primary text-[#d5d5d5] bg-transparent hover:bg-primary"><Download strokeWidth={2} />Resume</Button>
+            <Button className="rounded-full border-2 border-primary text-[#d5d5d5] bg-transparent hover:bg-primary cursor-pointer"><Download strokeWidth={2} />Resume</Button>
         </a>
     );
 };
